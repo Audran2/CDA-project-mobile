c243e6c611d3138e049770bc12387f089b03413e
Audran2 <audran.massacry@next-u.fr>
2023-11-28 09:09:50 +0100

    update : avancement home

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNav";
+import { useFonts } from "expo-font";
 
 const App = () => {
+  const [loaded] = useFonts({
+    KeaniaOne: require("./assets/fonts/KeaniaOne.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <AppNavigator />
--- components/homeScreen/Challenge.tsx
+++ components/homeScreen/Challenge.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import Svg, { Defs, Polygon, Pattern, Image } from "react-native-svg";
 
 export default function Challenge() {
   const { height, width } = Dimensions.get("window");
 
+  const imageSource = require("../../assets/images/trooper.jpg");
+
   return (
     <LinearGradient
-      style={{
-        height: height / 5 + 2,
-        width: width - 28,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 10,
-      }}
+      style={[
+        styles.borderLinear,
+        {
+          height: height / 5 + 2,
+          width: width - 28,
+        },
+      ]}
       start={{ x: 0, y: 1 }}
       end={{ x: 1, y: 0 }}
       colors={["#fff", "#696969"]}
@@ -24,22 +27,60 @@ export default function Challenge() {
           { height: height / 5, width: width - 30 },
         ]}
       >
-        <View
-          style={{ backgroundColor: "blue", width: "50%", height: "100%" }}
-        ></View>
-        <View
-          style={{ backgroundColor: "red", width: "50%", height: "100%" }}
-        ></View>
+        <View style={styles.centeredVersus}>
+          <Text style={{ fontFamily: "KeaniaOne", fontSize: 35 }}>VS</Text>
+        </View>
+
+        <Svg height={height / 5} width={width / 2}>
+          {/* <Defs>
+            <Pattern
+              id="image"
+              width={1}
+              height={1}
+              patternContentUnits="objectBoundingBox"
+            >
+              <Image
+                href={imageSource}
+                x="0"
+                y="0"
+                width="100%"
+                height="25%"
+                preserveAspectRatio="xMidYMid slice"
+              />
+            </Pattern>
+          </Defs> */}
+          <Polygon
+            points={`0,0 0,${height} ${(3 * width) / 6},0 ${width / 5},0`}
+            // fill="url(#image)"
+            fill="lightgray"
+          />
+        </Svg>
       </View>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  borderLinear: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+
   challengeContainer: {
+    position: "relative",
     backgroundColor: "white",
     borderRadius: 10,
     overflow: "hidden",
     flexDirection: "row",
+  },
+
+  centeredVersus: {
+    zIndex: 1,
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: [{ translateX: -25 }, { translateY: -25 }],
   },
 });
